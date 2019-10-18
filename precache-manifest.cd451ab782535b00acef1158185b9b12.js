024f1bb005438429c78e2bd161e902284a20ba6f
Tossapon Nuanchuay <ntossapo@gmail.com>
2019-10-18 21:48:16 +0700

    Updates

---
+++ precache-manifest.cd451ab782535b00acef1158185b9b12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76b334024e1c0241f5c84e6a41714c96",
+    "url": "/soft-biometric/index.html"
+  },
+  {
+    "revision": "413c176fbfe62d28b279",
+    "url": "/soft-biometric/static/css/main.b0851273.chunk.css"
+  },
+  {
+    "revision": "cac0cbc7b790a379e3d7",
+    "url": "/soft-biometric/static/js/2.2c6b1317.chunk.js"
+  },
+  {
+    "revision": "413c176fbfe62d28b279",
+    "url": "/soft-biometric/static/js/main.f5ad0e90.chunk.js"
+  },
+  {
+    "revision": "bb3512d23e585508a5a0",
+    "url": "/soft-biometric/static/js/runtime-main.94a5a932.js"
+  }
+]);
